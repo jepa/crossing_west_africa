186ef8eb342fa6fa063a9e4ba6b9ae36da4ae38c
Juliano Palacios Abrantes <j.palacios@oceans.ubc.ca>
2024-04-13 08:12:48 -0700

    Solve for cases where speveis is not founf in future

--- functions/summary_prop_fx.R
+++ functions/summary_prop_fx.R
@@ -1,8 +1,13 @@
 
 # Function modified from Palacios-Abrantes et acl (2020) code.
-# Estimates the SSR per species
+# Estimates the SSR per species by combining the routine including SummaryProp and PropChange
+
+taxon_key = 600228
 
 summary_ssr <- function(taxon_key){
+  
+  print(taxon_key)
+  # SummaryProp
   
     # Reads taxon data
     proportion_data <- fread(paste(my_path("G",extra_path = "EmergingFish/Results/Proportion_2005c/"),"proportion_",taxon_key,".csv", sep="")) %>% 
@@ -24,8 +29,12 @@ summary_ssr <- function(taxon_key){
     left_join(proportion_data) %>% 
     # Only keeps change if surpasses treshold
     mutate(
-      over_tresh = ifelse(time_step != "historical" & ensemble_mean > top_tresh | ensemble_mean < low_tresh,"keep",
-                          ifelse(time_step == "historical","keep","drop")
+      over_tresh = ifelse(time_step != "historical" & ensemble_mean > top_tresh | time_step != "historical" & ensemble_mean < low_tresh,"keep",
+                          ifelse(time_step == "historical","keep",
+                                 "drop")
+      ),
+      time_step = ifelse(time_step == "early","time_early",
+      ifelse(time_step == "mid","time_mid","historical")
       )
     ) %>% 
     # filter(time_step !="historical" | over_tresh == "keep") %>%
@@ -33,12 +42,15 @@ summary_ssr <- function(taxon_key){
     select(-top_tresh,-low_tresh)
   
     # summarizes by country
-    country_summary <- proportion_tresh %>% 
-      group_by(taxon_key,eez_name,eez_neighbour,time_step) %>% 
-      summarise(
-        mean_spp = mean(ensemble_mean,na.rm=T) # average spp change from all countries sharing that speceies
-      ) %>% 
-      filter(!is.na(mean_spp))
+  country_summary <- proportion_tresh %>% 
+    group_by(taxon_key,eez_name,eez_neighbour,time_step) %>% 
+    summarise(
+      mean_spp = mean(ensemble_mean,na.rm=T) # average spp change from all countries sharing that speceies
+    ) %>% 
+    filter(!is.na(mean_spp)) %>% 
+    spread(time_step,mean_spp) %>% 
+    mutate_at(vars(matches("time")), ~round((.-historical)/(abs(historical))*100,2))
   
+    
   return(country_summary)
 }
